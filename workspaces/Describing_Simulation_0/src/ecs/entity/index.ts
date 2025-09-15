bb66d5433e8c0b2aebb38bf65832a86ddc3ee09a
George Luo <formal.email.gl@gmail.com>
2025-09-15 14:45:03 -0700

    Add concrete entity implementations

---
+++ workspaces/Describing_Simulation_0/src/ecs/entity/index.ts
@@ -0,0 +1,4 @@
+export { Entity, EntityId } from "./Entity";
+export { EntityManager } from "./EntityManager";
+export { ConcreteEntity } from "./ConcreteEntity";
+export { ConcreteEntityManager } from "./ConcreteEntityManager";
